6242fc8f963ecc9fe3126bb0133247b448b46e86
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-01-19 22:25:53 +0700

    Developed header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 import './styles/index.scss'
-import { Header } from './components/Header'
-document.getElementById('root').appendChild(Header())
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true
   },
   mode: 'development',
   module: {
-    rules: [{
-      test: /\.(s*)css$/,
-      use: [
-        miniCss.loader,
-        'css-loader',
-        'sass-loader',
-      ]
-    },
-    {
-      test: /\.png/,
-      type: 'asset/resource'
-    }],
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          miniCss.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      }],
   },
   plugins: [
     new miniCss({
       filename: 'style.css',
     }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
   ]
 }
