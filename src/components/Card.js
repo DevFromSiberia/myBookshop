2d70eb19a65ec276c16bd3fbff23ddacf7ffe3a3
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-01-28 19:22:38 +0700

    rating display added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
-import { formatAuthor, formatDescr, formatPrice } from './Format'
+import { formatAuthor, formatDescr, formatPrice, fomatAvRate } from './Format'
 import placeholderImg from '../img/placeholderImg.png'
 
 function Card(book) {
-  let authors = formatAuthor(book.volumeInfo.authors)
-  let title = book.volumeInfo.title
-  let description = formatDescr(book.volumeInfo.description)
-  let price = formatPrice(book.saleInfo.listPrice)
-  let imageUrl = book.volumeInfo.imageLinks.thumbnail ? book.volumeInfo.imageLinks.thumbnail : placeholderImg
-  let id = book.id
+  const authors = formatAuthor(book.volumeInfo.authors)
+  const title = book.volumeInfo.title
+  const description = formatDescr(book.volumeInfo.description)
+  const price = formatPrice(book.saleInfo.listPrice)
+  const imageUrl = book.volumeInfo.imageLinks.thumbnail ? book.volumeInfo.imageLinks.thumbnail : placeholderImg
+  const id = book.id
+  const averageRating = fomatAvRate(book.volumeInfo.averageRating) ? fomatAvRate(book.volumeInfo.averageRating) : ''
+  const ratingCount = book.volumeInfo.ratingsCount ? `${book.volumeInfo.ratingsCount}  revies` : ""
 
   const html = `<li class="books__item" data-id="${id}">
   <div style="background-image: url(${imageUrl});" class="cover"></div>
@@ -15,22 +17,8 @@ function Card(book) {
     <p class="author">${authors}</p>
     <h2 class="title">${title}</h2>
     <div class="rate">
-      <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 0L7.80568 3.5147L11.7063 4.1459L8.92165 6.9493L9.52671 10.8541L6 9.072L2.47329 10.8541L3.07835 6.9493L0.293661 4.1459L4.19432 3.5147L6 0Z" fill="#EEEDF5"/>
-      </svg>
-      <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 0L7.80568 3.5147L11.7063 4.1459L8.92165 6.9493L9.52671 10.8541L6 9.072L2.47329 10.8541L3.07835 6.9493L0.293661 4.1459L4.19432 3.5147L6 0Z" fill="#EEEDF5"/>
-      </svg>
-      <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 0L7.80568 3.5147L11.7063 4.1459L8.92165 6.9493L9.52671 10.8541L6 9.072L2.47329 10.8541L3.07835 6.9493L0.293661 4.1459L4.19432 3.5147L6 0Z" fill="#EEEDF5"/>
-      </svg>
-      <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 0L7.80568 3.5147L11.7063 4.1459L8.92165 6.9493L9.52671 10.8541L6 9.072L2.47329 10.8541L3.07835 6.9493L0.293661 4.1459L4.19432 3.5147L6 0Z" fill="#EEEDF5"/>
-      </svg>
-      <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 0L7.80568 3.5147L11.7063 4.1459L8.92165 6.9493L9.52671 10.8541L6 9.072L2.47329 10.8541L3.07835 6.9493L0.293661 4.1459L4.19432 3.5147L6 0Z" fill="#EEEDF5"/>
-      </svg> 
-      <span>252 revies</span>
+      ${averageRating}
+      <span>${ratingCount}</span>
     </div>
     <p class="text">${description}</p>
     <div class="price">${price}</div>
