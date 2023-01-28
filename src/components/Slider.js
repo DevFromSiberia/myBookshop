356671dfd434a5a514da2d873a61ac3767e66167
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-01-28 14:31:50 +0700

    Added slider autoplay

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,14 @@
-import Swiper, { Pagination } from 'swiper';
+import Swiper, { Pagination, Autoplay } from 'swiper';
 
 const swiper = new Swiper('.swiper', {
-  modules: [Pagination],
+  modules: [Pagination, Autoplay],
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+
+  autoplay: {
+    delay: 5000,
+  },
 
   // If we need pagination
   pagination: {
